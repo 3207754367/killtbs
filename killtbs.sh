9cedc33f4b38b12615b8be442299185d629b9610
3207754367 <3207754367@qq.com>
2019-05-08 20:59:59 +0800

    一键去除tbs内核

--- killtbs.sh
+++ killtbs.sh
@@ -2,6 +2,7 @@
 
 chmod +x killtbs.sh
 qqmusic=/data/data/com.tencent.qqmusic/app_tbs
+qqmusic_Ad=/data/media/0/Android/data/com.tencent.qqmusic/cache/file/image
 wx=/data/data/com.tencent.mm/app_tbs
 qq=/data/data/com.tencent.mobileqq/app_tbs
 qqlive=/data/data/com.tencent.qqlive/app_tbs
@@ -15,5 +16,6 @@ elif [ -d $qq ] ; then
 elif [ -d $qqlive ] ; then
 	rm -rf $qqlive && touch $qqlive && chattr +i $qqlive
 else
-	echo "done"
+	rm -rf $qqmusic_Ad && touch $qqmusic_Ad && chattr +i $qqmusic_Ad
+	echo 去除tbs内核完成,顺便去除了QQ音乐启动页广告
 fi
