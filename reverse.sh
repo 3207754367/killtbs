f888193c5d3844c618c890aec3f70cac3ea50c50
3207754367 <3207754367@qq.com>
2019-05-10 19:47:36 +0800

    一键撤销只读属性

--- reverse.sh
+++ reverse.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+
 chmod +x reverse.sh
+
 qqmusic_tbs=/data/data/com.tencent.qqmusic/app_tbs
 qqmusic_Ad=/data/media/0/qqmusic/splash
 qqmusic_Ad2=/data/media/0/Android/data/com.tencent.qqmusic/sdcard/qqmusic/splash
@@ -19,21 +21,20 @@ else
 fi
 
 if [ ! -d "$qqsplash_Ad" ] ; then
-        qqsplash_Ad=/data/media/0/Android/data/com.tencent.mobileqq/sdcard/tencent/MobileQQ/splahAD
+        qqsplash_Ad=/data/media/0/Android/data/com.tencent.mobileqq/sdcard/tencent/MobileQQ/splahAD 
 else
         qqsplash_Ad2=/data/media/0/tencent/MobileQQ/splahAD
 fi
 
 if [ ! -d "$qqAd_Sdk" ] ;then
-        qqAd_Sdk=/data/media/0/Android/data/com.tencent.mob
-ileqq/sdcard/tencent/TMAssistantSDK
+        qqAd_Sdk=/data/media/0/Android/data/com.tencent.mobileqq/sdcard/tencent/TMAssistantSDK
 else
-        qqAd_Sdk2=/data/media/0/tencent/MobileQQ/splahAD
+        qqAd_Sdk2=/data/media/0/tencent/TMAssistantSDK
 fi
 isroot() {
 if [ `whoami` = "root" ];then
         echo "您是root用户，载入脚本...！"
-	[ -O  $qq ] && chattr -i $qqmusic_tbs $qqmusic_Ad $qqmusic_Ad2 $wx_tbs $qq_tbs $qqAd_Sdk $qqsplash_Ad $qqsplash_Ad2 $qqlive_tbs $douyin_splash && echo "只读属性撤销完成" 
+	[ -O  $qq ] && chattr -i $qqmusic_tbs $qqmusic_Ad $qqmusic_Ad2 $wx_tbs $qq_tbs $qqAd_Sdk $qqAd_Sdk2 $qqsplash_Ad $qqsplash_Ad2 $qqlive_tbs $douyin_splash && echo "只读属性撤销完成" 
 else
         echo "您是非root用户！, 脚本使用了系统管理员权限的Chattr命令,请提权后再运行本脚本" && exit
 fi
