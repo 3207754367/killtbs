d3d415907c6295c46379170d5bd6c8aac694732f
3207754367 <3207754367@qq.com>
2019-05-11 07:54:43 +0800

    一键去除tbs内核

--- killtbs.sh
+++ killtbs.sh
@@ -8,14 +8,11 @@ wx_tbs=/data/data/com.tencent.mm/app_tbs
 qq_tbs=/data/data/com.tencent.mobileqq/app_tbs
 qqAd_Sdk=/data/media/0/tencent/TMAssistantSDK
 qqAd_Sdk2=/data/media/0/Android/data/com.tencent.mobileqq/sdcard/tencent/TMAssistantSDK
-#qqsplash_Ad=/data/media/0/tencent/MobileQQ/splahAD
-#qqsplash_Ad2=/data/media/0/Android/data/com.tencent.mobileqq/sdcard/tencent/MobileQQ/splahAD
 qqbubble=/data/data/com.tencent.mobileqq/files/bubble_info
 qqfont=/data/media/0/tencent/MobileQQ/.font_info
 qqfont2=/data/media/0/Android/data/com.tencent.mobileqq/sdcard/tencent/MobileQQ/.font_info
 qqlive_tbs=/data/data/com.tencent.qqlive/app_tbs
 douyin_splash=/data/media/0/Android/data/com.ss.android.ugc.aweme/splashCache
-
 
 if [ ! -d $qqmusic_Ad ] ; then                
 	qqmusic_Ad=/data/media/0/Android/data/com.tencent.qqmusic/sdcard/qqmusic/splash
