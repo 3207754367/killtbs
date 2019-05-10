7818ff57ab2d8c3813922beada76c7a9745e882c
3207754367 <3207754367@qq.com>
2019-05-11 07:49:48 +0800

    一键撤销只读属性

--- killtbs.sh
+++ killtbs.sh
@@ -2,7 +2,7 @@
 
 chmod +x killtbs.sh
 qqmusic_tbs=/data/data/com.tencent.qqmusic/app_tbs
-qqmusic_Ad=/data/media/0/qqmusic/splash
+pqqmusic_Ad=/data/media/0/qqmusic/splash
 qqmusic_Ad2=/data/media/0/Android/data/com.tencent.qqmusic/sdcard/qqmusic/splash
 wx_tbs=/data/data/com.tencent.mm/app_tbs
 qq_tbs=/data/data/com.tencent.mobileqq/app_tbs
@@ -10,8 +10,12 @@ qqAd_Sdk=/data/media/0/tencent/TMAssistantSDK
 qqAd_Sdk2=/data/media/0/Android/data/com.tencent.mobileqq/sdcard/tencent/TMAssistantSDK
 #qqsplash_Ad=/data/media/0/tencent/MobileQQ/splahAD
 #qqsplash_Ad2=/data/media/0/Android/data/com.tencent.mobileqq/sdcard/tencent/MobileQQ/splahAD
+qqbubble=/data/data/com.tencent.mobileqq/files/bubble_info
+qqfont=/data/media/0/tencent/MobileQQ/.font_info
+qqfont2=/data/media/0/Android/data/com.tencent.mobileqq/sdcard/tencent/MobileQQ/.font_info
 qqlive_tbs=/data/data/com.tencent.qqlive/app_tbs
 douyin_splash=/data/media/0/Android/data/com.ss.android.ugc.aweme/splashCache
+
 
 if [ ! -d $qqmusic_Ad ] ; then                
 	qqmusic_Ad=/data/media/0/Android/data/com.tencent.qqmusic/sdcard/qqmusic/splash
@@ -25,16 +29,20 @@ else
 	qqAd_Sdk2=/data/media/0/tencent/MobileQQ/splahAD
 fi
 
+if [ ! -d $qqfont ] ; then
+	qqfont=/data/media/0/Android/data/com.tencent.mobileqq/sdcard/tencent/MobileQQ/.font_info
+else 
+	qqfont2=/data/media/0/tencent/MobileQQ/.font_info
+fi
+
 offroot () {
-	
-[ -O  $qq_tbs ]  && chattr +i $qqmusic_tbs $qqmusic_Ad $qqmusic_Ad2 $wx_tbs $qq_tbs $qqAd_Sdk $qqlive_tbs $douyin_splash  && echo "加入只读属性"
+chattr +i $qqmusic_tbs $qqmusic_Ad $qqmusic_Ad2 $wx_tbs $qq_tbs $qqAd_Sdk $qqfont $qqfont2 $qqlive_tbs $douyin_splash $qqbubble && echo "加入只读属性"
 
 }
 
-unroot (){
-chmod 000  $qqmusic_tbs $qqmusic_Ad $qqmusic_Ad2 $wx_tbs $qq_tbs $qqAd_Sdk $qqlive_tbs $douyin_splash && echo "设置权限为000"
+#unroot (){
+#chmod 000  $qqmusic_tbs $qqmusic_Ad $qqmusic_Ad2 $wx_tbs $qq_tbs $qqAd_Sdk $qqlive_tbs $douyin_splash $qqbubble  && echo "设置权限为000" }
 
-}
 removetbs (){
 echo -e "
 这是一个去除tbs内核的脚本
@@ -57,10 +65,16 @@ elif [ -d $qqAd_Sdk2 ] ; then
 	rm -rf $qqAd_Sdk2 && touch $qqAd_Sdk2
 elif [ -d $qqlive_tbs ] ; then
 	rm -rf $qqlive_tbs && touch $qqlive_tbs
+elif [ -d $qqbubble ] ; then
+	rm -rf $qqbubble && touch $qqbubble
+elif [ -d $qqfont ] ; then
+	rm -rf $qqfont && touch $qqfont
+elif [ -d $qqfont2 ] ; then
+	rm -rf $qqfont2 && touch $qqfont2
 elif [ -d $douyin_splash ] ; then
 	rm -rf $douyin_splash && touch $douyin_splash
 else
-	echo  "完成." &&  $istype
+	 $istype  && echo  完成
 	fi
 }
 
@@ -69,7 +83,10 @@ if [ `whoami` = "root" ];then
 	echo "您是root用户，载入脚本...！" && istype=offroot &&  removetbs
 
 else
-	echo "您是非root用户！, 脚本无法使用系统管理员权限的Chattr命令,正在启动免root方案..." && istype=unroot && removetbs 
+	echo "
+	您是非root用户！, 脚本无法使用系统管理员权限的Chattr命令...
+	请执行 'su -c ./killtbs.sh'
+	" && exit
 fi
 }
 
